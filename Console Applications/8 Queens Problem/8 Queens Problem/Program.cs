75a6a64a4bbfc2dce386d358d5cab6714d25a5a8
gopinathdee <gopinath_duraiswamy@hotmail.com>
2019-04-22 20:14:49 -0600

    Initial Version
    Initial Version "Game of Life" (Console Application)

---
+++ Console Applications/8 Queens Problem/8 Queens Problem/Program.cs
@@ -0,0 +1,90 @@
+﻿/*
+    Colorful Console - http://colorfulconsole.com/ 
+*/
+
+using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Console = Colorful.Console;
+
+namespace _8_Queens_Problem
+{
+    class Program
+    {
+        static int squaresInRowInChessBoard = 8;
+        static int numberOfQueensPlaced;
+        static bool[,] ChessBoard = new bool[8, 8];
+        static void Main(string[] args)
+        {
+            Solve8QueensProblem();
+            PrintGameOfLifeArray(); // Printing the Array
+            Console.ReadKey();
+        }
+
+        public static void Solve8QueensProblem()
+        {
+            numberOfQueensPlaced = 0;
+            PlaceQueenOnLocation(0, 0);
+        }
+
+        public static void PlaceQueenOnLocation(int rowIndex, int columnIndex)
+        {
+            int[,] nextValidLocationFromCurrent = new int[2, 1];
+
+            // Place the queen on the location
+            ChessBoard[rowIndex, columnIndex] = true;
+            numberOfQueensPlaced++;
+
+            // Identify location on the next row to place the next queen
+            nextValidLocationFromCurrent = IdentifyNextValidLocationFromCurrent(rowIndex, columnIndex);
+
+            // Exit criteria
+            if (numberOfQueensPlaced == 8) return;
+
+            // Recursively call Place the queen on the new location
+            PlaceQueenOnLocation(nextValidLocationFromCurrent[0, 0], nextValidLocationFromCurrent[1, 0]);
+        }
+
+        public static int[,] IdentifyNextValidLocationFromCurrent(int rowIndex, int columnIndex)
+        {
+            int[,] nextValidLocation = { { -1 }, { -1 } };
+
+            for (int intColumnCounter = columnIndex + 1; intColumnCounter < squaresInRowInChessBoard; intColumnCounter++)
+            {
+                nextValidLocation[0, 0] = rowIndex + 1;
+                nextValidLocation[1, 0] = intColumnCounter;
+
+                return nextValidLocation;
+            }
+            return nextValidLocation;
+        }
+
+        public static void PrintGameOfLifeArray()
+        {
+            for (int intRows = 0; intRows < ChessBoard.GetLength(0); intRows++)
+            {
+                for (int intColumns = 0; intColumns < ChessBoard.GetLength(1); intColumns++)
+                {
+                    FormatBooleanToText(ChessBoard[intRows, intColumns]);
+                }
+                Console.WriteLine();Console.WriteLine(); Console.WriteLine();
+            }
+        }
+
+        public static void FormatBooleanToText(bool ArrayValue)
+        {
+            if (ArrayValue)
+            {
+                Console.Write("     " + "QUEEN", Color.Green);
+            }
+            else
+            {
+                Console.Write("     " + "EMPTY ", Color.Red);
+            }
+        }
+
+    }
+}
---
+++ Console Applications/Game Of Life/GameOfLife/GameOfLife/Program.cs
@@ -0,0 +1,162 @@
+﻿/*
+    Colorful Console - http://colorfulconsole.com/ 
+*/
+using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Console = Colorful.Console;
+
+namespace GameOfLife
+{
+    class Program
+    {
+        static bool[,] GameOfLifeArray = new bool[10, 10];
+        static bool[,] GameOfLifeArrayProcessStatus;
+
+        static void Main(string[] args) {
+            InitializeGameOfLifeArray();
+            PrintGameOfLifeArray(); // Printing the Array
+
+            while (true)
+            {
+                GameOfLifeArrayProcessStatus = new bool[10, 10]; // Re-Initializing the procesing flag array
+                ProcessGameOfLifeArray();
+                PrintGameOfLifeArray();
+
+                Console.WriteLine("");
+                Console.Write("Do you want to process another generation ('Y' or 'Any Key'): ");
+
+                var keyPressed = Console.ReadKey();
+                if (keyPressed.Key != ConsoleKey.Y)
+                {
+                    Environment.Exit(0);
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        public static void InitializeGameOfLifeArray()
+        {
+            GameOfLifeArray[0, 1] = true;
+            GameOfLifeArray[1, 2] = true;
+            GameOfLifeArray[2, 0] = true;
+            GameOfLifeArray[2, 1] = true;
+            GameOfLifeArray[2, 2] = true;
+        }
+
+        public static void PrintGameOfLifeArray()
+        {
+            Console.WriteLine("========================================= {0} ======================================================", DateTime.Now);
+            for (int intRows = 0; intRows < GameOfLifeArray.GetLength(0); intRows++)
+            {
+                for (int intColumns = 0; intColumns < GameOfLifeArray.GetLength(1); intColumns++)
+                {
+                    FormatBooleanToText(GameOfLifeArray[intRows, intColumns]);
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        public static void ProcessGameOfLifeArray()
+        {
+            ProcessGameOfLifeCell(0, 0);
+        }
+
+        public static void ProcessGameOfLifeCell(int rowIndex, int columnIndex)
+        {
+            bool isCurrentlyAlive;
+            int[,] nextCellToProcess = new int[2, 1]; /* Array that will store the indexes of the next cell that is to be processed */
+
+            // Determine number of neighbours
+            int numberOfLiveNeighbours;
+
+            isCurrentlyAlive = GameOfLifeArray[rowIndex, columnIndex];
+            numberOfLiveNeighbours = DetermineNumberOfNeighbours(rowIndex, columnIndex);
+
+            nextCellToProcess = DetermineNextCellToProcess(rowIndex, columnIndex);
+
+            //Console.WriteLine ("Currently Processing ({0}, {1}). Will process ({2}, {3}) next", rowIndex, columnIndex, nextCellToProcess[0, 0], nextCellToProcess[1, 0]);
+
+            // Exit criteria for the Recursive function. If there are no cells to process, (-1, -1) will be returned
+            if ((nextCellToProcess[0, 0] == -1) && nextCellToProcess[1, 0] == -1) return;
+
+            // Recursively call to process the Next Cell
+            ProcessGameOfLifeCell(nextCellToProcess[0,0], nextCellToProcess[1,0]);
+            GameOfLifeArray[rowIndex, columnIndex] = ((isCurrentlyAlive) && ((numberOfLiveNeighbours == 2) || (numberOfLiveNeighbours == 3))) || (!isCurrentlyAlive && (numberOfLiveNeighbours == 3));
+        }
+
+        public static int DetermineNumberOfNeighbours(int rowIndex, int columnIndex)
+        {
+            int numberOfAliveNeighbours = 0;
+            bool cellStatus;
+
+            for (int intRowCounter = rowIndex - 1; intRowCounter <= rowIndex + 1; intRowCounter++)
+            {
+                for (int intColumnCounter = columnIndex - 1; intColumnCounter <= columnIndex + 1; intColumnCounter++)
+                {
+                    if ((intRowCounter == rowIndex) && (intColumnCounter == columnIndex))
+                    { }
+                    else
+                    {
+                        try
+                        {
+                            cellStatus = GameOfLifeArray[intRowCounter, intColumnCounter];
+                            if (cellStatus)
+                            {
+                                numberOfAliveNeighbours++;
+                            }
+                        }
+                        catch { } // Ignoring the "Index Out of Bounds" error
+                    }
+                }
+            }
+
+            GameOfLifeArrayProcessStatus[rowIndex, columnIndex] = true; // Mark the cell as processed, so that it is not processed again
+            return numberOfAliveNeighbours;
+        }
+
+        public static int[,] DetermineNextCellToProcess(int rowIndex, int columnIndex)
+        {
+            int[,] nextCellToProcess = {{-1},{-1}};
+            bool cellStatus;
+
+            for (int intRowCounter = rowIndex - 1; intRowCounter <= rowIndex + 1; intRowCounter++)
+            {
+                for (int intColumnCounter = columnIndex - 1; intColumnCounter <= columnIndex + 1; intColumnCounter++)
+                {
+                    if ((intRowCounter == rowIndex) && (intColumnCounter == columnIndex))
+                    { }
+                    else
+                    {
+                        try
+                        {
+                            cellStatus = GameOfLifeArray[intRowCounter, intColumnCounter];
+                            if (!GameOfLifeArrayProcessStatus[intRowCounter, intColumnCounter])
+                            {
+                                nextCellToProcess[0, 0] = intRowCounter;
+                                nextCellToProcess[1, 0] = intColumnCounter;
+                                return nextCellToProcess;
+                            }
+                        }
+                        catch { } // Ignoring the "Index Out of Bounds" error
+                    }
+                }
+            }
+            return nextCellToProcess;
+        }
+
+        public static void FormatBooleanToText(bool ArrayValue)
+        {
+            if(ArrayValue) {
+                Console.Write("    " + "LIFE ", Color.Green);
+            }
+            else {
+                Console.Write("    " + "EMPTY", Color.Red);
+            }
+        }
+    }
+    //public enum 
+}
